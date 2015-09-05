f3686086e0c2e065c0d75188ca04e1dce9589fed
ikukhar <ivkukhar@gmail.com>
2015-09-05 23:07:28 +0300

    Remove social buttons

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-    #  user.email = auth.info.email if auth.info.email && !auth.info.email.blank?
+     user.email = auth.info.email if auth.info.email && !auth.info.email.blank?
      user.password = Devise.friendly_token[0,20]
      user.username = auth.info.name   # assuming the user model has a name
     end
