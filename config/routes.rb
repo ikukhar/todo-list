f7b0866ccff36c1224238fe88cd1cf62b7b716d6
ikukhar <ivkukhar@gmail.com>
2015-09-05 22:51:14 +0300

    Add path for omniauth callbacks

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,13 @@ Rails.application.routes.draw do
 
   root 'page#index'
 
-  namespace :users do
-    get 'omniauth_callbacks/github'
-  end
-
-  namespace :users do
-    get 'omniauth_callbacks/vkontakte'
-  end
-
   devise_scope :user do
     delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session_path
   end
 
-  devise_for :users, controllers: { sessions: "users/sessions" }
+  devise_for :users, controllers: { sessions: "users/sessions", :omniauth_callbacks => "users/omniauth_callbacks"}
 
   resources :projects, only: [:create, :update, :destroy]
   resources :tasks,    only: [:create, :update, :destroy]
-  
+
 end
